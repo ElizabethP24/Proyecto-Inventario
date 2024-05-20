d8f43cb9478770ee9cbb7a12f06a5be9b4136930
ElizabethP <112828461+ElizabethP24@users.noreply.github.com>
2024-05-19 21:33:38 -0500

    registros en base de datos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from werkzeug.utils import secure_filename
 
 # Crear la aplicación Flask y configurar la carpeta estática
 app = Flask(__name__, 
@@ -17,6 +18,14 @@ conexion = mysql.connector.connect(
     password="",
     database="huella_amor"
 )
+
+# Configuración de la carpeta de subida
+UPLOAD_FOLDER = 'ProyectoInventario/static/assets/img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Definir una ruta para la página principal
 @app.route('/')
@@ -50,15 +59,6 @@ def login():
 def home():
     return render_template('home.html')
 
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
-@app.route('/categories')
-def categories():
-    return render_template('categories.html')
-@app.route('/client')
-def client():
-    return render_template('client.html')
 @app.route('/inventory')
 def inventory():
     return render_template('inventory.html')
@@ -71,6 +71,27 @@ def providers():
     proveedores = listar_proveedores()
     # Renderizar la plantilla con los datos de los proveedores
     return render_template('providers.html', proveedores=proveedores)
+
+@app.route('/categories')
+def categories():
+    # Llamar a la función que obtiene las categorias
+    categorias = listar_categorias()
+    # Renderizar la plantilla con los datos de las categorias
+    return render_template('categories.html', categorias=categorias)
+
+@app.route('/admin')
+def admin():
+    # Llamar a la función que obtiene los administradores
+    administradores = listar_administradores()
+    # Renderizar la plantilla con los datos de los administradores
+    return render_template('admin.html', administradores=administradores)
+
+@app.route('/client')
+def client():
+    # Llamar a la función que obtiene los clientes
+    clientes= listar_clientes()
+    # Renderizar la plantilla con los datos de los clientes
+    return render_template('client.html', clientes=clientes)
 
 @app.route('/sales')
 def sales():
@@ -104,6 +125,157 @@ def guardar_proveedor():
         flash("Error al insertar proveedor: {}".format(error), "error")
         return redirect(url_for('providers'))
     
+    
+@app.route('/guardar_categoria', methods=['POST'])
+def guardar_categoria():
+    # Obtener los datos del formulario
+    nombre = request.form['NameCategory']
+    descripcion = request.form['descriptionCategory']
+        
+    # Preparar la consulta de inserción
+    consulta = "INSERT INTO categorias (nombrecat, descripcioncat) VALUES (%s, %s)"
+    valores = (nombre, descripcion)
+    
+    try:
+        # Crear un cursor y ejecutar la consulta
+        cursor = conexion.cursor()
+        cursor.execute(consulta, valores)
+        conexion.commit()
+        flash("Categoría agregado exitosamente", "success")
+        # Cerrar el cursor
+        cursor.close()
+        return redirect(url_for('categories'))
+    except mysql.connector.Error as error:
+        print("Error al insertar categoria:", error)
+        conexion.rollback()
+        flash("Error al insertar categoria: {}".format(error), "error")
+        return redirect(url_for('categories'))
+    
+@app.route('/guardar_administrador', methods=['POST'])
+def guardar_administrador():
+    # Obtener los datos del formulario
+    documento = request.form['DNIAdmin']
+    nombre = request.form['NameAdmin']
+    telefono = request.form['phoneAdmin']
+    correo = request.form['emailAdmin']
+    usuario = request.form['UserNameAdmin']
+    contrasena = request.form['passwordAdmin']
+    avatar = request.form['avatarAdmin']
+    
+        
+    # Preparar la consulta de inserción
+    consulta = "INSERT INTO administradores (docadmin, nombreadmin, telefonoadmin, correoadmin, usuarioadmin, contrasenaadmin, avataradmin) VALUES (%s, %s,%s, %s,%s, %s,%s)"
+    valores = (documento, nombre, telefono, correo, usuario, contrasena, avatar)
+    
+    try:
+        # Crear un cursor y ejecutar la consulta
+        cursor = conexion.cursor()
+        cursor.execute(consulta, valores)
+        conexion.commit()
+        flash("Administrador agregado exitosamente", "success")
+        # Cerrar el cursor
+        cursor.close()
+        return redirect(url_for('admin'))
+    except mysql.connector.Error as error:
+        print("Error al insertar administrador:", error)
+        conexion.rollback()
+        flash("Error al insertar administrador: {}".format(error), "error")
+        return redirect(url_for('admin'))
+    
+@app.route('/guardar_cliente', methods=['POST'])
+def guardar_cliente():
+    # Obtener los datos del formulario
+    dni = request.form['DNIClient']
+    nombre = request.form['NameClient']
+    direccion = request.form['addressClient']
+    telefono = request.form['phoneClient']
+    correo = request.form['emailClient']
+        
+    # Preparar la consulta de inserción
+    consulta = "INSERT INTO clientes (doccliente,nombrecliente, direccioncliente,telefonocliente,correocliente ) VALUES (%s, %s, %s, %s, %s)"
+    valores = (dni, nombre, direccion, telefono, correo)
+    
+    try:
+        # Crear un cursor y ejecutar la consulta
+        cursor = conexion.cursor()
+        cursor.execute(consulta, valores)
+        conexion.commit()
+        flash("Cliente agregado exitosamente", "success")
+        # Cerrar el cursor
+        cursor.close()
+        return redirect(url_for('client'))
+    except mysql.connector.Error as error:
+        print("Error al insertar cliente:", error)
+        conexion.rollback()
+        flash("Error al insertar cliente: {}".format(error), "error")
+        return redirect(url_for('client'))
+    
+def ejecutar_consulta(consulta, valores):
+    try:
+        if not conexion.is_connected():
+            conexion.connect()  # Reconnect if connection is closed
+        cursor = conexion.cursor()
+        cursor.execute(consulta, valores)
+        conexion.commit()
+        cursor.close()
+        return True
+    except mysql.connector.Error as error:
+        print("Error al ejecutar consulta:", error)
+        conexion.rollback()
+        return False
+    
+@app.route('/guardar_producto', methods=['POST'])
+def guardar_producto():
+    # Obtener los datos del formulario
+    codigo = request.form['CodeProduct']
+    nombre = request.form['NameProduct']
+    unidades = request.form['StockProduct']
+    precio = request.form['PriceProduct']
+    categoria = request.form['CategoryProduct']
+    proveedor = request.form['provider_id']
+    descripcion = request.form['DescriptionProduct']
+    marca = request.form['markProduct']
+    fecha = request.form['dateProduct']
+    estado = request.form['statusProduct']
+    imagen = request.files['fileProduct']  # Obtener el objeto de archivo completo
+        
+    # Preparar la consulta de inserción
+    consulta = "INSERT INTO productos (idproductos, nombreprod, unidadesprod, precioprod, categoriaprod, proveedorprod, descripcionprod, marcaprod, fecharegistro, estatusprod, imagenprod ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    valores = (codigo, nombre, unidades, precio, categoria, proveedor, descripcion, marca, fecha, estado, imagen.filename)  # Guardar solo el nombre del archivo en la base de datos
+    
+    try:
+        if ejecutar_consulta(consulta, valores):
+            # Pasar el archivo completo a la función upload_product()
+            upload_product(imagen)
+            flash("Producto agregado exitosamente", "success")
+        else:
+            flash("Error al insertar producto", "error")
+    except Exception as e:
+        print("Error:", e)
+        flash("Error al insertar producto", "error")
+    
+    return redirect(url_for('products'))
+
+def upload_product(file):
+    if file.filename == '':
+        flash('No se ha seleccionado ningún archivo')
+        return redirect(url_for('products'))  
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        
+        # Verificar si el directorio existe, y si no, crearlo
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        
+        file.save(filepath)
+        
+        flash('Archivo subido exitosamente', 'success')
+        return redirect(url_for('products'))
+    else:
+        flash('Tipo de archivo no permitido', 'error')
+        return redirect(url_for('products'))
     
 def listar_proveedores():
     print("Entrando en la función listar_proveedores()")
@@ -143,6 +315,142 @@ def listar_proveedores():
             conexion.close()
     
     return proveedores
+
+def listar_categorias():
+    print("Entrando en la función listar_categorias()")
+    categorias = []
+    try:
+        # Establecer la conexión a la base de datos
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="huella_amor"
+        )
+        print("Conexión establecida correctamente")
+        # Crear un cursor para ejecutar consultas SQL
+        cursor = conexion.cursor()
+
+        print("Ejecutando consulta SQL")
+        # Ejecutar una consulta para obtener todos las categorias
+        cursor.execute("""
+            SELECT nombrecat, descripcioncat
+            FROM categorias
+        """)
+
+        # Obtener todos los registros las categorias
+        categorias = cursor.fetchall()
+        print("Categorias obtenidos:", categorias)  # Verificar los datos en la consola
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        categorias = []
+    
+    finally:
+        # Cerrar el cursor y la conexión en la sección finally
+        if cursor:
+            cursor.close()
+        if conexion:
+            conexion.close()
+    
+    return categorias
+
+def listar_administradores():
+    print("Entrando en la función listar_administradores()")
+    administradores = []
+    try:
+        # Establecer la conexión a la base de datos
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="huella_amor"
+        )
+        print("Conexión establecida correctamente")
+        # Crear un cursor para ejecutar consultas SQL
+        cursor = conexion.cursor()
+
+        print("Ejecutando consulta SQL")
+        # Ejecutar una consulta para obtener todos las categorias
+        cursor.execute("""
+            SELECT docadmin, nombreadmin, telefonoadmin, correoadmin, avataradmin
+            FROM administradores
+        """)
+
+        # Obtener todos los registros las categorias
+        administradores = cursor.fetchall()
+        print("Administradores obtenidos:", administradores)  # Verificar los datos en la consola
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        administradores = []
+    
+    finally:
+        # Cerrar el cursor y la conexión en la sección finally
+        if cursor:
+            cursor.close()
+        if conexion:
+            conexion.close()
+    
+    return administradores
+
+def listar_clientes():
+    print("Entrando en la función listar_clientes()")
+    clientes = []
+    try:
+        # Establecer la conexión a la base de datos
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="huella_amor"
+        )
+        print("Conexión establecida correctamente")
+        # Crear un cursor para ejecutar consultas SQL
+        cursor = conexion.cursor()
+
+        print("Ejecutando consulta SQL")
+        # Ejecutar una consulta para obtener todos los clientes
+        cursor.execute("""
+            SELECT doccliente, nombrecliente, direccioncliente, telefonocliente, correocliente 
+            FROM clientes
+        """)
+
+        # Obtener todos los registros de clientes
+        proveedores = cursor.fetchall()
+        print("Clientes obtenidos:", clientes)  # Verificar los datos en la consola
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        clientes = []
+    
+    finally:
+        # Cerrar el cursor y la conexión en la sección finally
+        if cursor:
+            cursor.close()
+        if conexion:
+            conexion.close()
+    
+    return proveedores
+
+@app.route('/validate_provider/<provider_id>', methods=['GET'])
+def validate_provider(provider_id):
+    exists = check_provider_exists(provider_id)
+    return jsonify({'exists': exists})
+
+def check_provider_exists(provider_id):
+    # Conéctate a tu base de datos y realiza la consulta para verificar si el proveedor existe
+    cursor = conexion.cursor()
+    cursor.execute("SELECT COUNT(*) FROM proveedores WHERE docproveedores = %s", (provider_id,))
+    result = cursor.fetchone()
+    cursor.close()
+    conexion.close()
+    return result[0] > 0
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     # Ejecutar la aplicación Flask
